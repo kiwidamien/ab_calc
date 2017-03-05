d4c83e8a50c76c1958606f909e268db98db82506
kiwidamien <djmartin@ucdavis.edu>
2017-03-04 23:49:10 -0800

    First section layout finished

--- setup_questions.js
+++ setup_questions.js
@@ -39,6 +39,19 @@ $(document).ready(
   }
 );
 
+function resetN(){
+  var alphaRate = $("#alphaInput").val();
+  var betaRate  = $("#betaInput").val();
+  var baserate  = $("#baserateInput").val();
+  var min_diff  = $("#diffInput").val();
+
+  var N         = sampleSizeNeeded(baserate/100, min_diff/100, alphaRate / 100, betaRate / 100);
+
+  $("#trialInput").val(N);
+  $("#trialInputSlider").slider('value',N);
+
+  doSetupUpdate(true);
+}
 
 
 function doSetupUpdate(updateN){
@@ -55,13 +68,19 @@ function doSetupUpdate(updateN){
 
   var sampleSizeInput   = $('#trialInput');
   var currentSampleSize = sampleSizeInput.val();
+  var msgDiv = $("#trialNumberSuggest");
+
+  var linkHTML = "<a onclick='resetN();' style='color:blue; text-decoration: underline;'>Click to set to recommended number.</a>";
 
   if (currentSampleSize < 0.95*N){
     sampleSizeInput.css('color','red');
+    msgDiv.html("Warning: trials selected (" + currentSampleSize + ") less than suggested (" + N + "). " + linkHTML );
   } else if (currentSampleSize > 1.05*N){
     sampleSizeInput.css('color', 'green');
+    msgDiv.html("Warning: trials selected (" + currentSampleSize + ") significantly more than suggested (" + N + "). " + linkHTML);
   } else {
     sampleSizeInput.css('color', 'black');
+    msgDiv.text("");
   }
   $("#alternative_min").text(Number(baserate)+Number(min_diff));
   $('#generateData').text("Continue with " + currentSampleSize + " trials");
